18fe3e793caf2f573b3e5b6b970ab4ecf22b68c9
majakk <mattias.jacobsson@gmail.com>
2012-06-16 15:38:10 +0200

    Change-Id: I5dc8f48dc6b9f17e191b6e7bbcd46f2ea389ba0e

---
+++ diylwpbase/src/studio/coldstream/diylwpbase/LiveWallpaperSettings.java
@@ -0,0 +1,46 @@
+package studio.coldstream.diylwpbase;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+//import android.util.Log;
+
+
+public class LiveWallpaperSettings extends PreferenceActivity implements SharedPreferences.OnSharedPreferenceChangeListener
+{	
+	
+	
+	@Override
+    protected void onCreate(Bundle icicle)
+	{
+        super.onCreate(icicle);
+        getPreferenceManager().setSharedPreferencesName(LiveWallpaper.SHARED_PREFS_NAME);
+        addPreferencesFromResource(R.xml.wallpaper_settings);
+        getPreferenceManager().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);       
+    }
+
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+    }
+
+    @Override
+    protected void onDestroy()
+    {
+        getPreferenceManager().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+        super.onDestroy();
+    }
+
+	@Override
+	public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key)
+	{
+		
+	}
+
+	
+	
+	
+	
+	
+}
